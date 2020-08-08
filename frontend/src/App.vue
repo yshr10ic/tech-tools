3d77f12cc7546f8a1482b9815835e99129162242
yshr10ic <yshr10ic@gmail.com>
2020-08-08 15:23:40 +0900

    不要ファイルの削除

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -38,20 +38,16 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
 
   data: () => ({
     //
---
+++ frontend/src/views/PasswordGenerator.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Password Generator</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PasswordGenerator',
+}
+</script>
